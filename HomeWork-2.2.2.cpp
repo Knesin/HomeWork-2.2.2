62cae0849e909d5397803ee0e27e1e9d71892d94
Maksim Kl <GuruHacker@yandex.ru>
2023-01-26 23:31:30 +0300

    Initial commit

---
+++ HomeWork-2.2.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+struct account {
+    unsigned int number;
+    std::string name;
+    double money;
+};
+
+void changeMoney(account& user, const double &new_money)
+{
+    user.money = new_money;
+}
+
+int main()
+{
+    account user1;
+    double in_money;
+
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> user1.number;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> user1.name;
+    std::cout << "Введите баланс: ";  
+    std::cin >> user1.money;
+    std::cout << "Введите новый баланс : ";
+    std::cin >> in_money;
+    changeMoney(user1, in_money);
+    std::cout << "Ваш счёт: " << user1.name << ", " << user1.number << ", " << user1.money << std::endl;
+
+}
